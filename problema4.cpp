8b7ff431d4e02183f6e04e55949145f290679168
sebas2503 <54363976+sebas2503@users.noreply.github.com>
2023-03-22 18:06:02 -0500

    Add files via upload

---
+++ problema4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+string pi = "14159265358979";
+string resu = "3,";
+int main()
+{
+  int n = 0;
+  cout << "Hello, world: ";
+  cin>>n;
+  cout<<endl;
+  if(int(pi[n]) >= 5)
+  {
+    int a = (int(pi[n-1])+1)-48;
+    pi[n-1] = a+'0';
+  }
+  for(int x = 0;x < n;x++)
+  {
+    resu += pi[x];
+  }  
+  cout<<"Pi: "<<resu<<endl;
+  return 0;
+}
